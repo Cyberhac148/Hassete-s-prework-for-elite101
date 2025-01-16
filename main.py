d4bcd83476e233d40aa3402d1d1ae5b8238ecf31
Cyberhac148 <168587927+Cyberhac148@users.noreply.github.com>
2025-01-16 04:26:24 +0000

    Adding input varibles

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello! Welcone to intersteller chat box!")
 name = input("Well...Hello Earthling, what your name?")
+age = input(name+ " ,may I also ask how long have you lived in your planet?")
 
